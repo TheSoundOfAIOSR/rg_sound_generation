14da721147b9f544be5f75be97deab9eaced2074
Amit Yadav <amit.yadav.iitr@gmail.com>
2021-03-23 10:29:01 +0000

    annotator changes

--- audio_annotator/audio_annotator/qualities.py
+++ audio_annotator/audio_annotator/qualities.py
@@ -1,5 +1,5 @@
 qualities = ('bright', 'dark', 'full', 'hollow', 'smooth', 'rough', 'warm', 'metallic',
              'clear', 'muddy', 'thin', 'thick', 'pure', 'noisy', 'rich', 'sparse',
-             'harmonic', 'disharmonic', 'soft', 'hard')
+             'soft', 'hard')
 
 pairs = [(qualities[i], qualities[i + 1]) for i in range(0, len(qualities), 2)]
