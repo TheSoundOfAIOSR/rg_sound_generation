ca234736115d1c8fee248cecd23e2ecb3645fa4e
Amit Yadav <amit.yadav.iitr@gmail.com>
2021-07-04 13:08:39 +0100

    updated loss

--- timbre_conditioned_vae/tcvae/losses.py
+++ timbre_conditioned_vae/tcvae/losses.py
@@ -2,12 +2,22 @@ import tensorflow as tf
 from .localconfig import LocalConfig
 
 
+# def harmonic_loss(h, reconstruction, mask):
+#     f_pred, m_pred = tf.unstack(reconstruction, axis=-1)
+#     f_gt, m_gt = tf.unstack(h, axis=-1)
+#     m_loss = tf.square(m_pred - m_gt)
+#     f_loss = tf.math.multiply(tf.square(f_pred - f_gt), m_gt)
+#     reconstruction_loss = m_loss + f_loss * LocalConfig().freq_loss_weight
+#     reconstruction_loss = tf.math.multiply(reconstruction_loss, mask)
+#     reconstruction_loss = tf.reduce_sum(reconstruction_loss) / tf.reduce_sum(mask)
+#     return reconstruction_loss
+
 def harmonic_loss(h, reconstruction, mask):
     f_pred, m_pred = tf.unstack(reconstruction, axis=-1)
     f_gt, m_gt = tf.unstack(h, axis=-1)
-    m_loss = tf.square(m_pred - m_gt)
-    f_loss = tf.math.multiply(tf.square(f_pred - f_gt), m_gt)
-    reconstruction_loss = m_loss + f_loss * LocalConfig().freq_loss_weight
-    reconstruction_loss = tf.math.multiply(reconstruction_loss, mask)
-    reconstruction_loss = tf.reduce_sum(reconstruction_loss) / tf.reduce_sum(mask)
+    m_loss = tf.reduce_sum(tf.square(m_pred - m_gt) * mask) / tf.reduce_sum(mask)
+    # this normalization complete the weighted sum before it was divided by tf.reduce_sum(mask)
+    # instead of tf.reduce_sum(m_gt * mask)
+    f_loss = tf.reduce_sum(tf.square(f_pred - f_gt) * (m_gt * mask)) / tf.reduce_sum(m_gt * mask)
+    reconstruction_loss = m_loss + f_loss
     return reconstruction_loss
