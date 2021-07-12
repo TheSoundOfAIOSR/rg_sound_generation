2bbebb9386ae4bed7119ef9350e31d3939356084
Amit Yadav <amit.yadav.iitr@gmail.com>
2021-07-12 14:23:35 +0100

    changed train from cli

--- timbre_conditioned_vae/train_from_cli.py
+++ timbre_conditioned_vae/train_from_cli.py
@@ -4,10 +4,15 @@ from tcvae import localconfig, train
 if __name__ == "__main__":
     conf = localconfig.LocalConfig()
 
-    conf.batch_size = 1
-    conf.epochs = 1
+    conf.dataset_dir = "/content/drive/MyDrive/the_sound_of_ai/new_data"
+    conf.checkpoints_dir = "/content/drive/MyDrive/the_sound_of_ai/new_data/checkpoints"
     conf.use_encoder = False
     conf.decoder_type = "cnn"
-    conf.step_log_interval = 2
+    conf.batch_size = 8
+    conf.learning_rate = 2e-3
+    conf.model_name = "cnn_decoder_heuristics"
+    conf.step_log_interval = 10
+    conf.reconstruction_weight = 1.
+    conf.save_config()
 
     train.train(conf)
