77a0e982c539d1d5e4aa8f70758fe0ca13f3b9fe
Fabio Di Marco <fabiodimarco@hotmail.it>
2021-07-16 00:53:35 +0200

    Modified CompleteTFRecordProvider to remove dependency from DDSP

--- timbre_conditioned_vae/tcvae/tfrecord_provider.py
+++ timbre_conditioned_vae/tcvae/tfrecord_provider.py
@@ -1,36 +1,47 @@
 import tensorflow as tf
-import ddsp.training.data as data
 
 
-class CompleteTFRecordProvider(data.RecordProvider):
+class CompleteTFRecordProvider:
     def __init__(self,
-                 file_pattern=None,
+                 file_pattern,
                  example_secs=4,
                  sample_rate=16000,
                  frame_rate=250,
                  map_func=None):
-        super().__init__(file_pattern, example_secs, sample_rate,
-                         frame_rate, tf.data.TFRecordDataset)
+        self._file_pattern = file_pattern
+        self._sample_rate = sample_rate
+        self._frame_rate = frame_rate
+        self._audio_length = example_secs * sample_rate
+        self._feature_length = example_secs * frame_rate
+        self._data_format_map_fn = tf.data.TFRecordDataset
         self._map_func = map_func
 
     def get_dataset(self, shuffle=True):
-      def parse_tfexample(record):
-        features = tf.io.parse_single_example(record, self.features_dict)
-        if self._map_func is not None:
-          return self._map_func(features)
-        else:
-          return features
+        def parse_tfexample(record):
+            features = tf.io.parse_single_example(record, self.features_dict)
+            if self._map_func is not None:
+                return self._map_func(features)
+            else:
+                return features
 
-      filenames = tf.data.Dataset.list_files(self._file_pattern, shuffle=shuffle)
-      dataset = filenames.interleave(
-          map_func=self._data_format_map_fn,
-          cycle_length=40,
-          num_parallel_calls=tf.data.experimental.AUTOTUNE,
-          deterministic=True)
-      dataset = dataset.map(parse_tfexample,
-                            num_parallel_calls=tf.data.experimental.AUTOTUNE,
-                            deterministic=True)
-      return dataset
+        filenames = tf.data.Dataset.list_files(self._file_pattern,
+                                               shuffle=shuffle)
+        dataset = filenames.interleave(
+            map_func=self._data_format_map_fn,
+            cycle_length=40,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+            deterministic=True)
+        dataset = dataset.map(parse_tfexample,
+                              num_parallel_calls=tf.data.experimental.AUTOTUNE,
+                              deterministic=True)
+        return dataset
+
+    def get_batch(self, batch_size, shuffle=True, repeats=-1):
+        dataset = self.get_dataset(shuffle)
+        dataset = dataset.repeat(repeats)
+        dataset = dataset.batch(batch_size, drop_remainder=True)
+        dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        return dataset
 
     @property
     def features_dict(self):
