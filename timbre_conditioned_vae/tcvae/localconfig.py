8c4c6723e28a2017b46b389bcba00fc3d8678b75
Amit Yadav <amit.yadav.iitr@gmail.com>
2021-07-17 15:03:14 +0100

    conf singleton required

--- timbre_conditioned_vae/tcvae/localconfig.py
+++ timbre_conditioned_vae/tcvae/localconfig.py
@@ -79,7 +79,7 @@ class LocalConfig:
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(LocalConfig, cls).__new__()
+            cls._instance = super(LocalConfig, cls).__new__(cls)
         return cls._instance
 
     def set_config(self, params: Dict):
