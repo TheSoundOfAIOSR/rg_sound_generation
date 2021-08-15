cbebffbd725fa8ad3781df4007283df7e1cef4c3
Amit Yadav <amit.yadav.iitr@gmail.com>
2021-08-15 23:57:27 +0100

    sg app known latent samples

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import random
 import soundfile as sf
 from time import time
 from loguru import logger
@@ -11,36 +12,72 @@ def load_sound_generator():
     return SoundGenerator()
 
 
+@st.cache(allow_output_mutation=True)
+def get_known_zs():
+    _known_zs = []
+    with open("latent_samples.csv", "r") as f:
+        _data = f.read().splitlines()
+        _data = _data[1:]
+        for row in _data:
+            _known_zs.append([int(float(x) * 100) for x in row.split(",")])
+    return _known_zs
+
+
+known_zs = get_known_zs()
 measure_max_val = 149
 z_max_val = 99
-default_z = 50
+default_z = [50] * 5
+default_m = [0] * 11
 
-st.title("Sound Generator")
-
-z1 = st.sidebar.slider("z1", min_value=0, max_value=z_max_val, value=default_z)
-z2 = st.sidebar.slider("z2", min_value=0, max_value=z_max_val, value=default_z)
-z3 = st.sidebar.slider("z3", min_value=0, max_value=z_max_val, value=default_z)
-z4 = st.sidebar.slider("z4", min_value=0, max_value=z_max_val, value=default_z)
-z5 = st.sidebar.slider("z5", min_value=0, max_value=z_max_val, value=default_z)
-
-inharmonicity = st.sidebar.slider("inharmonicity", min_value=0, max_value=measure_max_val)
-even_odd = st.sidebar.slider("even_odd", min_value=0, max_value=measure_max_val)
-sparse_rich = st.sidebar.slider("sparse_rich", min_value=0, max_value=measure_max_val)
-attack_rms = st.sidebar.slider("attack_rms", min_value=0, max_value=measure_max_val)
-decay_rms = st.sidebar.slider("decay_rms", min_value=0, max_value=measure_max_val)
-attack_time = st.sidebar.slider("attack_time", min_value=0, max_value=measure_max_val)
-decay_time = st.sidebar.slider("decay_time", min_value=0, max_value=measure_max_val)
-bass = st.sidebar.slider("bass", min_value=0, max_value=measure_max_val)
-mid = st.sidebar.slider("mid", min_value=0, max_value=measure_max_val)
-high_mid = st.sidebar.slider("high_mid", min_value=0, max_value=measure_max_val)
-high = st.sidebar.slider("high", min_value=0, max_value=measure_max_val)
-
-input_pitch = st.slider("input_pitch", min_value=40, max_value=88, value=60)
-output_pitch = st.slider("output_pitch", min_value=40, max_value=88, value=60)
-velocity = st.slider("velocity", min_value=25, max_value=125, value=75, step=25)
+if "latent_sample" not in st.session_state:
+    st.session_state["latent_sample"] = default_z
+else:
+    default_z = st.session_state["latent_sample"]
 
 
-if st.button("Generate"):
+col1, col2, col3 = st.beta_columns(3)
+
+
+col1.title("Generator")
+col2.subheader("Latent Sample")
+col3.subheader("Measures")
+
+
+input_pitch = col1.slider("input_pitch", min_value=40, max_value=88, value=60)
+output_pitch = col1.slider("output_pitch", min_value=40, max_value=88, value=60)
+velocity = col1.slider("velocity", min_value=25, max_value=125, value=75, step=25)
+
+
+if col2.button("Reset Z"):
+    st.session_state.clear()
+    default_z = [50] * 5
+
+if col2.button("Get Suggested Z"):
+    default_z = random.choice(known_zs)
+    logger.info("Got suggested Z, updating session state")
+    st.session_state["latent_sample"] = default_z
+
+z1 = col2.slider("z1", min_value=0, max_value=z_max_val, value=default_z[0])
+z2 = col2.slider("z2", min_value=0, max_value=z_max_val, value=default_z[1])
+z3 = col2.slider("z3", min_value=0, max_value=z_max_val, value=default_z[2])
+z4 = col2.slider("z4", min_value=0, max_value=z_max_val, value=default_z[3])
+z5 = col2.slider("z5", min_value=0, max_value=z_max_val, value=default_z[4])
+
+
+inharmonicity = col3.slider("inharmonicity", min_value=0, max_value=measure_max_val, value=default_m[0])
+even_odd = col3.slider("even_odd", min_value=0, max_value=measure_max_val, value=default_m[1])
+sparse_rich = col3.slider("sparse_rich", min_value=0, max_value=measure_max_val, value=default_m[2])
+attack_rms = col3.slider("attack_rms", min_value=0, max_value=measure_max_val, value=default_m[3])
+decay_rms = col3.slider("decay_rms", min_value=0, max_value=measure_max_val, value=default_m[4])
+attack_time = col3.slider("attack_time", min_value=0, max_value=measure_max_val, value=default_m[5])
+decay_time = col3.slider("decay_time", min_value=0, max_value=measure_max_val, value=default_m[6])
+bass = col3.slider("bass", min_value=0, max_value=measure_max_val, value=default_m[7])
+mid = col3.slider("mid", min_value=0, max_value=measure_max_val, value=default_m[8])
+high_mid = col3.slider("high_mid", min_value=0, max_value=measure_max_val, value=default_m[9])
+high = col3.slider("high", min_value=0, max_value=measure_max_val, value=default_m[10])
+
+
+if col1.button("Generate"):
     z = [z / 100 for z in [z1, z2, z3, z4, z5]]
     measures = [m / 100 for m in [
         inharmonicity, even_odd, sparse_rich, attack_rms,
@@ -63,5 +100,5 @@ if st.button("Generate"):
         audio = np.array(audio) / np.max(np.abs(audio))
         sf.write("temp.wav", audio, samplerate=sg.conf.sample_rate)
 
-    st.subheader("Audio")
-    st.audio("temp.wav", format="audio/wav")
+    col1.subheader("Audio")
+    col1.audio("temp.wav", format="audio/wav")
