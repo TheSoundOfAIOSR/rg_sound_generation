670d7e10bd85d2f74e1d7f0cda99c34b077c9cd3
Amit Yadav <amit.yadav.iitr@gmail.com>
2021-10-03 16:58:59 +0100

    dataset prep

---
+++ dataset/run.py
@@ -0,0 +1,14 @@
+import warnings
+import prepare
+
+
+sets = ["train"]
+dataset_dir = "D:\soundofai\\nsynth-guitar-subset"
+checkpoints_dir = "D:\soundofai\\ddsp_trained_30k"
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+
+    for s in sets:
+        # prepare.prepare_partial_tfrecord(dataset_dir=dataset_dir, split=s)
+        prepare.prepare_complete_tfrecord(dataset_dir=dataset_dir, checkpoints_dir=checkpoints_dir, split=s)
